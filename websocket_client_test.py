d72d0a01bf3638dfd13118f43b33d017030c7e92
humeman <camdenjsf@gmail.com>
2021-05-22 01:50:20 -0600

    Add websocket client example

---
+++ websocket_client_test.py
@@ -0,0 +1,114 @@
+import websockets
+import asyncio
+import json
+
+class WebsocketClient:
+    def __init__(
+            self,
+            ip,
+            key: str = None
+        ):
+
+        self.socket = None
+
+        self.ip = ip
+
+        if key:
+            self.auth = {"key": key}
+
+        else:
+            self.auth = {}
+
+    async def start(
+            self
+        ):
+
+        self.socket = await websockets.connect(
+            self.ip
+        )
+
+        recv_task = asyncio.get_event_loop.create_task(self.recv())
+        send_task = asyncio.get_event_loop.create_task(self.send_loop())
+
+        done, pending = await asyncio.wait(
+            [recv_task, send_task],
+            return_when = asyncio.FIRST_COMPLETED
+        )
+
+        for task in pending:
+            task.cancel()
+
+        print("Done")
+
+    async def recv(
+            self
+        ):
+
+        async for message in self.socket:
+            print(message)
+
+    async def send_loop(
+            self
+        ):
+
+        while True:
+            await asyncio.sleep(1) 
+            # Don't exit, but we don't have to send anything at the moment.
+            # Janky, I know. Just might need to actually use this later.
+
+    async def set(
+            self,
+            motors: dict,
+            autostart: bool = False
+        ):
+
+        comp = [
+            {"id": motor_id, "state": state} for motor_id, state in motors.items()
+        ]
+
+        await self.socket.send(
+            json.dumps(
+                {
+                    "command": "set",
+                    "motors": comp,
+                    "autostart": autostart,
+                    **self.auth
+                }
+            )
+        )
+
+    async def standby(
+            self,
+            standby: bool
+        ):
+
+        await self.socket.send(
+            json.dumps(
+                {
+                    "command": "standby",
+                    "standby": standby,
+                    **self.auth
+                }
+            )
+        )
+
+    async def stop(
+            self
+        ):
+
+        await self.socket.send(
+            json.dumps(
+                {
+                    "command": "stop",
+                    **self.auth
+                }
+            )
+        )
+
+websocket = WebsocketClient(
+    "ws://localhost:5000",
+    key = "test"
+)
+
+asyncio.get_event_loop().run_until_complete(websocket.start())
+asyncio.get_event_loop().run_forever()
